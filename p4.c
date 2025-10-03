c1f618fa18d87064e9ca07cf3ed1e472809eeee0
KrishnaKarmude07 <kkarmude@gmail.com>
2025-10-03 00:00:31 -0700

    Create p4.c

---
+++ p4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int a,b;
+int main(){
+    int mul;
+    printf("Enter the numbers : ");
+    scanf("%d %d",&a,&b);
+
+     mul= a*b;
+         printf("multipication of %d and %d is = %d\n",a,b,mul);
+
+    
+          return 0 ; 
+}
